26eca1c3f3fb1970d4e24f7842483ac4c47551e5
Yuta <y.nakagawa1107@gmail.com>
2019-01-03 23:00:22 +0900

    卵作成

--- egg1/ViewController.swift
+++ egg1/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var messege: UILabel!
+    var num = 100
+    
+    
+    @IBAction func tapmainasu(_ sender: Any) {
+        num = num-10
+        label.text = String(num)
+        
+        
+        if num == 0{
+            messege.text = "終わりだよ"
+        }else  if num == 10{
+            messege.text = "おつかれさんw"
+        }else{
+            messege.text = ""
+        }
+        if num < 0{
+            label.text = ""
+        }
+    }
 }
+
+
+
+
 
