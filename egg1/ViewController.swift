6a26d75055ae1e9728cbb4bd093240e9c06dbe49
Yuta <y.nakagawa1107@gmail.com>
2019-01-03 22:03:53 +0900

    Initial Commit

---
+++ egg1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  egg1
+//
+//  Created by 中川裕太 on 2019/01/03.
+//  Copyright © 2019 yuta.nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
